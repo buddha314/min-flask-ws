f819428021949551a788d32ecf02d5cd4de8558e
buddha314 <buddha_314@yahoo.com>
2018-04-04 17:13:31 -0700

    okay working but probably not minimal

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
+from threading import Lock
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.DEBUG=True
+app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+thread = None
+thread_lock = Lock()
 
-
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count}, namespace='/test')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,22 +27,24 @@ def index():
 def test_connect():
     print("in connect")
     global thread
-    #with thread_lock:
-    #    if thread is None:
-    #        thread = socketio.start_background_task(target=background_thread)
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
     emit('my_response', {'data': 'Connected', 'count': 0})
 
 @socketio.on('my_event', namespace='/test')
 def test_message(message):
     print(" in test_message: %s" % message)
     session['receive_count'] = session.get('receive_count', 0) + 1
-    #session['receive_count'] = session.get('receive_count', 0) + 1
-    #emit('my_response', {'data': message['data'], 'count': session['receive_count']})
     emit('my_response', {'data': message['data'], 'count': session['receive_count']})
 
 @socketio.on('my_broadcast_event', namespace='/test')
-def handle_message(message):
-    print('received message: ' + message)
+def test_broadcast_message(message):
+    print('received message: %s' % message)
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']}, broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
